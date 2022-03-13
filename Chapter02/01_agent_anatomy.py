2a34b6326f13b8ab8a1fd3385ab70823c617dcf4
771739520 <49917590+771739520@users.noreply.github.com>
2022-03-14 01:48:50 +0800

    Update 01_agent_anatomy.py

--- Chapter02/01_agent_anatomy.py
+++ Chapter02/01_agent_anatomy.py
@@ -6,7 +6,7 @@ class Environment:
     def __init__(self):
         self.steps_left = 10
 
-    def get_observation(self) -> List[float]:
+    def get_observation(self) -> List[float]:#->是函数返回值的注释
         return [0.0, 0.0, 0.0]
 
     def get_actions(self) -> List[int]:
@@ -26,10 +26,10 @@ class Agent:
     def __init__(self):
         self.total_reward = 0.0
 
-    def step(self, env: Environment):
-        current_obs = env.get_observation()//这个变量是没有用的
-        actions = env.get_actions()
-        reward = env.action(random.choice(actions))
+    def step(self, env: Environment):#参数后的冒号是函数注释，如果有特定的值需要标记（https://cloud.tencent.com/developer/article/1573221）网站有详解
+        current_obs = env.get_observation()#这个变量是没有用的
+        actions = env.get_actions() #带上小括号就是调用函数了，这里就是调用了class Environment 里的get_actinons函数
+        reward = env.action(random.choice(actions))#actions是形参，里面的0或者1是实参。实参就是真实的数值，形参就是变量名
         self.total_reward += reward
 
 
